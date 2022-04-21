f9d0bfe468ce28f17e2a4ebffdf4be71fba0cea3
ashukifley12 <95753777+ashukifley12@users.noreply.github.com>
2022-04-21 20:27:12 +0300

    Update problem1_5.py

--- Programming Assignment/problem1_5.py
+++ Programming Assignment/problem1_5.py
@@ -1,7 +1,7 @@
 def problem1_5(age):
     if age < 7:
         print("Have a glass of milk")
-    elif age < 21:
+    elif age < 21 and age >=7:
         print("Have a coke")
     else :
         print("Have a martini")
